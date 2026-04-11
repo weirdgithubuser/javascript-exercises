0f3a525d4dc40d61ec11328c4ef21b6d65cc68bc
weirdgithubuser <96423455+weirdgithubuser@users.noreply.github.com>
2026-04-11 14:01:48 +0530

    complete exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const convertToCelsius = function(fahr) {
-  if (Number.isNaN) return false;
-  return Math.round((( fahr - 32) + (5/9)) * 10) / 10;
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
 
+const convertToCelsius = function(fahr) {
+  return round((( fahr - 32) * (5/9)), 1);
 };
 
 const convertToFahrenheit = function(cels) {
-  if (Number.isNaN) return false;
-  return Math.round((((9/5) * cels) + 32) * 10) / 10;
+  return round((((9/5) * cels) + 32),1) ;
 };
 
 // Do not edit below this line
