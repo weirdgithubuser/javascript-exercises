090a4e9ef89feb9dd5fcafdfba0678b0719ab1e2
weirdgithubuser <96423455+weirdgithubuser@users.noreply.github.com>
2026-04-11 13:30:51 +0530

    complete exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start > 0 && end > 0 && start < end &&
+        Math.floor(start) === start && Math.floor(end) === end){
+        let sum = start;
+        for (let i = start + 1; i <= end; i++){
+            sum += i;
+        }
+        return sum;
+    }else if (start > 0 && end > 0 && start > end &&
+        Math.floor(start) === start && Math.floor(end) === end){
+        let sum = end;
+        for (let i = end + 1; i <= start; i++){
+            sum += i;
+        }
+        return sum;
+    }else {
+        return "ERROR";
+    };
 
 };
 
