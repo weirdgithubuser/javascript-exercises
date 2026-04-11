da60c1507219cd24bed4be93ab8f1de39ed82e38
weirdgithubuser <96423455+weirdgithubuser@users.noreply.github.com>
2026-04-11 13:56:01 +0530

    complete exercise 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return false;
+    if (year % 4 === 0){
+        if (year % 100 === 0){
+            if (year % 400 === 0){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }else return false;
 
 };
 
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  if (Number.isNaN) return false;
+  return Math.round((( fahr - 32) + (5/9)) * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  if (Number.isNaN) return false;
+  return Math.round((((9/5) * cels) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
