e9d0123e8641081e27b01ca9b231ca3106ababec
weirdgithubuser <96423455+weirdgithubuser@users.noreply.github.com>
2026-04-25 19:37:33 +0530

    COMPLETE EXERCISE 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    oldest = arr[0];
+    for (let i = 0; i < arr.length; i++){
+        if(!(arr[i].yearOfDeath)){
+            arr[i].yearOfDeath = (new Date()).getFullYear();
+        }
+        if((arr[i].yearOfDeath - arr[i].yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)){
+            oldest = arr[i];
+        }
+    }
+    return oldest
 };
 
 // Do not edit below this line
