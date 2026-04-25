f4dbd1a11be75191b43022e35435998219d3396e
weirdgithubuser <96423455+weirdgithubuser@users.noreply.github.com>
2026-04-25 15:25:15 +0530

    COMPLETE QUESTION 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const arr = string.toLowerCase().split('').map(item => {
+        if(!(['!','.',',',' '].includes(item))){
+            return item
+        }
+    });
+    return (arr.join('') === arr.reverse().join(''))
 };
 
 // Do not edit below this line
