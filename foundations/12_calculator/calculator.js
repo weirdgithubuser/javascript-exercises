493e551d33a5310accf60e8749dfcbc05e9c06b2
weirdgithubuser <96423455+weirdgithubuser@users.noreply.github.com>
2026-04-25 13:38:45 +0530

    COMPLETE EXERCISE 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2); 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return Array.from(numbers).reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return Array.from(numbers).reduce((product, item) => product * item,1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+  let product = 1;
+  for (let i = num; i > 0; i--){
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
